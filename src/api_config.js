5bc1d6bf92dc04246c56d02814c39870de094463
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-05-31 18:28:44 +0300

    apiurl updated

--- src/api_config.js
+++ src/api_config.js
@@ -1,8 +1,8 @@
-//export const apiUrl = "https://api.fayidaacademy.com";
-export const apiUrl = "http://localhost:5000";
+export const apiUrl = "https://api.fayidaacademy.com";
+//export const apiUrl = "http://localhost:5000";
 
-//export const localUrl = "https://admin.fayidaacademy.com";
-export const localUrl = "http://localhost:3000";
+export const localUrl = "https://admin.fayidaacademy.com";
+//export const localUrl = "http://localhost:3000";
 
 export const adminId = "47b789e0-409f-43c5-9c21-d7a0df1dcd01";
 export const subAdminId = "2507060f-4698-41af-b958-4eacebeea3ad";
