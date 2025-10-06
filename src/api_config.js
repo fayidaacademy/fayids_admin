f78d494f881061318eb109c70923c0b675d7dfd2
Kidyoh <kidyoh789@gmail.com>
2025-10-06 13:04:25 +0300

    fix: Update apiUrl to use the production endpoint

--- src/api_config.js
+++ src/api_config.js
@@ -1,4 +1,4 @@
-export const apiUrl = "http://localhost:5000";
+export const apiUrl = "https://api.fayidaacademy.com";
 
 export const localUrl = "https://admin.fayidaacademy.com";
 // export const localUrl = "http://localhost:3000";
